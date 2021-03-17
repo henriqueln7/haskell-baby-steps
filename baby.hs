6d0e2366711273fc9775079696e05bee4629285c
Henrique Lopes <henriquelopees7@gmail.com>
2021-03-17 08:57:14 -0300

    Add some functions to baby.hs (Lists are cool)

--- baby.hs
+++ baby.hs
@@ -3,3 +3,9 @@ doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100 
                         then x 
                         else x*2;
+
+myReplicate x y = take x (repeat y)
+boomBangs xs = [if x < 10 then "BOOM" else "BANG" | x <- xs, odd x]
+length' xs = sum [1 | _ <- xs]
+removeUppercase string = [s | s <- string, not (s `elem` ['A'..'Z'])]
+removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
