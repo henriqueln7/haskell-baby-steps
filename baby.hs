fcc475247675b18e70bf09ec3abd7ef334e7156a
Henrique Lopes <henriquelopees7@gmail.com>
2021-03-19 09:08:45 -0300

    Type declarations

--- baby.hs
+++ baby.hs
@@ -8,4 +8,8 @@ myReplicate x y = take x (repeat y)
 boomBangs xs = [if x < 10 then "BOOM" else "BANG" | x <- xs, odd x]
 length' xs = sum [1 | _ <- xs]
 removeUppercase string = [s | s <- string, not (s `elem` ['A'..'Z'])]
+removeNonUppercase :: [Char] -> [Char]
 removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
+
+addThree :: Int -> Int -> Int -> Int;
+addThree x y z = x + y + z;
